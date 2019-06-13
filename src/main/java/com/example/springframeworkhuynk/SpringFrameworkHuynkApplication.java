b38dd23bc753f48d636ce1429bf09b2fa91b493b
khanhhuy101096 <khanhhuy101096@gmail.com>
2019-06-13 08:50:56 +0700

    Initial commit

---
+++ src/main/java/com/example/springframeworkhuynk/SpringFrameworkHuynkApplication.java
@@ -0,0 +1,13 @@
+package com.example.springframeworkhuynk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkHuynkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkHuynkApplication.class, args);
+    }
+
+}
